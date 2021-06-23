8323c623a084c6517fb30190a9604309d01a1a14
Leejha <57664427+Leejha@users.noreply.github.com>
2021-06-23 23:11:29 +0900

    feat : Not solve 1926 - baekjoon

--- 1926 - 그림.py
+++ 1926 - 그림.py
@@ -1,51 +1,52 @@
 from collections import deque
 
+def bfs(y, x) : 
+
+	sum = 0
+
+	que = deque([[y, x]])
+	visited[y][x] = True
+
+	while que :
+		v = que.popleft() 
+		sum += 1
+
+		for i in range(4) :
+			ny = v[0] + dy[i]
+			nx = v[1] + dx[i] 
+
+			if nx < 0 or nx >= m or ny < 0 or ny >= n :
+				continue
+
+			if drawing[ny][nx] == 1 and visited[ny][nx] == False :
+				que.append([ny, nx])
+				visited[ny][nx] = True
+
+	draw_pix.append(sum)
+
 n,m = map(int, input().split())
 
-image = [] 
+drawing = [] 
+
+draw_pix = []
+
 
 for i in range(n) :
 	pixel = list(map(int, input().split()))
-	image.append(pixel)
+	drawing.append(pixel)
 
-visited = [ [False] * m for _ in range(n)]
+visited = [[False] * m for _ in range(n)]
 
 dx = [-1, 1, 0 ,0]
 dy = [0, 0, -1, 1]
 
-def bfs_all (xy) :
-	que = deque([xy]) 
-	visited[xy[0]][xy[1]] = True
-
-	while que :
-		pix = que.popleft()
-
-		for i in range(4) :
-			x = pix[0] + dx 
-			y = pix[1] + dy
-
-			if  x < 0 or y < 0 or x >= n or y >= m :
-				continue
-
-			visited[x][y] = True
-
-			que.append((x,y))
 
 
-def bfs_area (xy) :
-	que = deque([xy]) 
-	visited[xy[0]][xy[1]] = True
+for y in range(n) :
+	for x in range(m) : 
+		if drawing[y][x] == 1 and visited[y][x] == False :
+			bfs(y,x) 
 
-	while que :
-		pix = que.popleft()
 
-		for i in range(4) :
-			x = pix[0] + dx 
-			y = pix[1] + dy
-
-			if  x < 0 or y < 0 or x >= n or y >= m :
-				continue
-
-			visited[x][y] = True
-
-			que.append((x,y))			
+print(len(draw_pix))
+print(max(draw_pix))
